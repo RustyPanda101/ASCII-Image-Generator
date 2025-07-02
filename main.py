fe8e1d44fc08e11fbaee130d09f48f3a700b7b39
Aditya Prasoon <adityaanil365@gmail.com>
2025-07-03 02:33:39 +0530

    works

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
-import time
 import numpy as np
 import cv2 as oc
 import math
 
-starTime = time.time()
 
-image = oc.imread('img.jpg')
-imageArray = np.array(image)
+image = oc.imread('flower.jpg')
+gray = oc.cvtColor(image, oc.COLOR_BGR2GRAY)
 
-#Lower index = higher brightness
+
+fac = 0.8
+gray_small = oc.resize(gray, (0, 0), fx=fac, fy=fac, interpolation=oc.INTER_AREA)
+
+#low index = higher brightness
 substitution = "Ñ@#W$9876543210?!abc "
 
-print("before :",imageArray[0][0])
-print(np.shape(imageArray))
 
-# shape : (951, 713, 3)
-for i in imageArray:
-    for j in i:
-        avg = math.floor(np.average(j, axis=0))
-        j[0]= avg
-        j[1] = avg
-        j[2] = avg
-print("after :", imageArray[0][0])
+h, w = gray_small.shape
+char_w, char_h = 10, 10
+asciiImage = np.zeros((h * char_h, w * char_w, 3), dtype=np.uint8)
 
-fac = 0.05
-imageArray = oc.resize(imageArray, (0,0),fx=fac, fy=fac, interpolation=oc.INTER_AREA)
-print(np.shape(imageArray))
 
+
+for i in range(h):
+    for j in range(w):
+        brightness = gray_small[i, j]
+        ind = (255 - int(brightness)) * len(substitution) // 256
+        char = substitution[ind]
+        x, y = j * char_w, i * char_h + char_h
+        oc.putText(asciiImage, char, (x, y), oc.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+
+
+
+oc.namedWindow('ASCII Art', oc.WINDOW_NORMAL)
+oc.imshow("ASCII Art", asciiImage)
+
+oc.namedWindow('Image', oc.WINDOW_NORMAL)
 oc.imshow("Image", image)
-oc.namedWindow('Average', oc.WINDOW_NORMAL)
-oc.imshow("Average", imageArray)
-endTime = time.time()
 
+oc.namedWindow('Average', oc.WINDOW_NORMAL)
+oc.imshow("Average", gray_small)
 
 oc.waitKey(0)
-print(f"Elapsed time:{endTime-starTime}")
+oc.destroyAllWindows()
