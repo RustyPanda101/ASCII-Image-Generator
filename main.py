7fe575a6afac8c145fa8049c4135eb6a8319ad62
Aditya Prasoon <adityaanil365@gmail.com>
2025-07-03 21:04:22 +0530

    added colour image capablities

--- main.py
+++ main.py
@@ -2,42 +2,55 @@ import numpy as np
 import cv2 as oc
 import math
 
-
-image = oc.imread('flower.jpg')
-gray = oc.cvtColor(image, oc.COLOR_BGR2GRAY)
-
-
-fac = 0.8
-gray_small = oc.resize(gray, (0, 0), fx=fac, fy=fac, interpolation=oc.INTER_AREA)
-
-#low index = higher brightness
 substitution = "Ñ@#W$9876543210?!abc "
 
+def imageWindow(asciiImage, image, gray_small):
+    oc.namedWindow('ASCII Art', oc.WINDOW_NORMAL)
+    oc.imshow("ASCII Art", asciiImage)
 
-h, w = gray_small.shape
-char_w, char_h = 10, 10
-asciiImage = np.zeros((h * char_h, w * char_w, 3), dtype=np.uint8)
+    oc.namedWindow('Image', oc.WINDOW_NORMAL)
+    oc.imshow("Image", image)
 
+    oc.namedWindow('Average', oc.WINDOW_NORMAL)
+    oc.imshow("Average", gray_small)
 
-
-for i in range(h):
-    for j in range(w):
-        brightness = gray_small[i, j]
-        ind = (255 - int(brightness)) * len(substitution) // 256
-        char = substitution[ind]
-        x, y = j * char_w, i * char_h + char_h
-        oc.putText(asciiImage, char, (x, y), oc.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+    oc.waitKey(0)
+    oc.destroyAllWindows()
 
 
+def monoChrome(image, fac):
+    gray = oc.cvtColor(image, oc.COLOR_BGR2GRAY)
+    gray_small = oc.resize(gray, (0, 0), fx=fac, fy=fac, interpolation=oc.INTER_AREA)
+    h, w = gray_small.shape
+    char_w, char_h = 10, 10
+    asciiImage = np.zeros((h * char_h, w * char_w, 3), dtype=np.uint8)
 
-oc.namedWindow('ASCII Art', oc.WINDOW_NORMAL)
-oc.imshow("ASCII Art", asciiImage)
+    for i in range(h):
+        for j in range(w):
+            brightness = gray_small[i, j]
+            ind = (255 - int(brightness)) * len(substitution) // 256
+            char = substitution[ind]
+            x, y = j * char_w, i * char_h + char_h
+            oc.putText(asciiImage, char, (x, y), oc.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+    imageWindow(asciiImage, image, gray_small)
 
-oc.namedWindow('Image', oc.WINDOW_NORMAL)
-oc.imshow("Image", image)
 
-oc.namedWindow('Average', oc.WINDOW_NORMAL)
-oc.imshow("Average", gray_small)
+def colourImage(image, fac):
+    image_small = oc.resize(image, (0, 0), fx=fac, fy=fac, interpolation=oc.INTER_AREA)
+    h, w, z = image_small.shape
+    char_w, char_h = 10, 10
+    asciiImage = np.zeros((h * char_h, w * char_w, 3), dtype=np.uint8)
 
-oc.waitKey(0)
-oc.destroyAllWindows()
+    for i in range(h):
+        for j in range(w):
+            brightness = (image_small[i, j][0] + image_small[i, j][1] + image_small[i, j][2])/3
+            ind = (255 - int(brightness)) * len(substitution) // 256
+            char = substitution[ind]
+            x, y = j * char_w, i * char_h + char_h
+            print(tuple(map(int,image_small[i,j])))
+            oc.putText(asciiImage, char, (x, y), oc.FONT_HERSHEY_PLAIN, 1,tuple(map(int,image_small[i,j])),1)
+    imageWindow(asciiImage, image, image_small)
+
+
+image = oc.imread('img_2.png')
+colourImage(image, 0.1)
